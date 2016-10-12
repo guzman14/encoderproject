97d5aa68d92a224a15f16bfc29684909c2f254a5
Gerardo Guzman <jerryguzman873@gmail.com>
2016-10-12 09:10:52 -0700

    first commmit

---
+++ encoder.js
@@ -0,0 +1,12 @@
+$("#divname").keydown(function(e) {
+  //code to execute when user presses a key goes here
+}
+
+$("input[type='radio']").______(function() {
+  $("input:checked").prop('checked', ______);
+  $(this).prop('checked', _____);
+});
+
+if ($("input:checked").val() == "echo") {
+  //code here
+}
