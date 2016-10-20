259f1e3011c1f669e25a638ede246f5ed129c990
Gerardo Guzman <jerryguzman873@gmail.com>
2016-10-20 07:58:51 -0700

    started caeser

--- encoder.js
+++ encoder.js
@@ -12,3 +12,21 @@ var letters = {
       var inputText = document.getElementById("inputText").value;
    document.getElementById("textArea").innerHTML = inputText;
 });
+
+  else if ($("input:checked").val() == "caesar") {
+
+    var ex = document.getElementById('inputText').value;
+
+  var y = ex.length + 1;
+
+  var minus = x - 1;
+
+  for(var x = 0; x < y; x++) {
+    var minus = x - 1;
+    var z = ex.substring(minus, x);
+    console.log(z);
+    $("#textArea").append(caeserConvert[z]);
+    document.getElementById('inputText').value = "";
+    //loops through conversion and outputs each character individually
+
+  }
